fafc035d8ded9994de1b88464074377f2a63b821
oiyoi420 <klimashenko.2017@gmail.com>
2025-05-22 01:01:33 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,1 @@
-import type {NextConfig} from 'next';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable static export
-  output: 'export',
-
-  // Add any other configuration options for Next.js
-  experimental: {
-    appDir: false, // Disable app directory if using incompatible features
-  },
-};
-
-module.exports = nextConfig;
-
-export default nextConfig;
